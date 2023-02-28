84a0c059583d5ff767b396619536e163b3e5976b
Hyunwoo Jang <bkcort@naver.com>
2023-02-28 14:07:01 +0900

    20230227

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pickle
 import numpy as np
 import math
 import time
@@ -12,6 +13,43 @@ baudrate = 115200
 
 print('port: ', port)
 print('baudrate: ', baudrate)
+
+save_path = r'C:\Users\user\PycharmProjects\threading\ecmo_ai_model_221007.pickle'
+
+with open(save_path, 'rb') as f:
+    trainingdb = pickle.load(f)
+
+w1 = trainingdb['heart']['w1']
+w2 = trainingdb['heart']['w2']
+w3 = trainingdb['heart']['w3']
+w4 = trainingdb['ecmo']['w4']
+w5 = trainingdb['ecmo']['w5']
+w6 = trainingdb['ecmo']['w6']
+
+b1 = trainingdb['heart']['b1']
+b2 = trainingdb['heart']['b2']
+b3 = trainingdb['heart']['b3']
+b4 = trainingdb['ecmo']['b4']
+b5 = trainingdb['ecmo']['b5']
+b6 = trainingdb['ecmo']['b6']
+
+
+def NDivision(arr, n, ref):
+    outp = np.array([])
+    for i in range(0, len(arr)):
+        if i % n == ref:
+            outp = np.append(outp, arr[i])
+    return outp
+
+
+def DNN(x, h1_w, h1_b, h2_w, h2_b, o_w, o_b):
+    def Dnn_Relu(arr):
+        for i in range(0, arr.shape[0]):
+            if arr[i] < 0:
+                arr[i] = 0
+            else:
+                pass
+        return arr
 
 
 try:
@@ -29,7 +67,7 @@ try:
             self.running = True
 
         def run(self):
-            global R
+            global R, ibp_val, diff_ibp
             while self.running:
 
                 if ser.readable():
@@ -54,7 +92,43 @@ try:
 
                                     ibp_val = (int(ibp_h, 16) & int('01111111', 2)) * (2 ** 7) + \
                                               (int(ibp_l, 16) & int('01111111', 2)) - 512
-                                    print('ibp_val:', ibp_val)
+
+                                    # print('ibp_val:', ibp_val)
+
+                                    if ibp_val > 0:
+                                        diff_ibp = np.diff(ibp_val)
+
+                                    print('diff_data:', diff_ibp)
+
+                                    # # diff 필요한 범위만 가져온 뒤 표준화
+                                    # diff_inp = ibp_val[i:i + 125]
+                                    # save_diff_val = np.array(diff_inp)
+                                    # diff_inp_min = np.min(diff_inp)
+                                    # diff_inp_max = np.max(diff_inp)
+                                    # diff_inp = (diff_inp - diff_inp_min) / (diff_inp_max - diff_inp_min)
+                                    #
+                                    # # DNN 적용
+                                    # outp_h = DNN(ibp_val, w1, b1, w2, b2, w3, b3)
+                                    # outp_e = DNN(ibp_val, w4, b4, w5, b5, w6, b6)
+                                    #
+                                    # # DNN 출력값 중 마지막 값(파형 없음을 나타내는) 제거
+                                    # outp_h = np.array(outp_h[0:-1])
+                                    # outp_e = np.array(outp_e[0:-1])
+                                    #
+                                    # # 출력값 누적
+                                    # if i == 0:
+                                    #     save_diff = np.array(save_diff_val)
+                                    #
+                                    #     save_outp_h = np.hstack((np.zeros(65), outp_h, np.zeros(30)))
+                                    #     save_outp_e = np.hstack((np.zeros(65), outp_e, np.zeros(30)))
+                                    # else:
+                                    #     save_diff = np.append(save_diff, save_diff_val[-1])
+                                    #
+                                    #     save_outp_h = np.append(save_outp_h, 0)
+                                    #     save_outp_h[-60:-30] = save_outp_h[-60:-30] + outp_h
+                                    #
+                                    #     save_outp_e = np.append(save_outp_e, 0)
+                                    #     save_outp_e[-60:-30] = save_outp_e[-60:-30] + outp_e
 
                                     j_ref = 1
                                     break
