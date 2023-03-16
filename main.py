9ab389fb8f4d6f00b99aa574789094a9a8d6f1e6
HHHH <109564232+helios3000@users.noreply.github.com>
2023-03-16 16:34:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1379,3 +1379,434 @@ my_program = MyProgram()
 my_program.run()
 # time.sleep(1)
 
+
+
+
+##########20230316#############
+
+
+
+
+import pickle
+import numpy as np
+from math import factorial
+import time
+import serial
+import threading
+
+port = 'COM8'
+baudrate = 115200
+
+print('port: ', port)
+print('baudrate: ', baudrate)
+
+save_path = r'C:\Users\HW\PycharmProjects\threading\ecmo_ai_model_221007.pickle'
+
+with open(save_path, 'rb') as f:
+    trainingdb = pickle.load(f)
+
+w1 = trainingdb['heart']['w1']
+w2 = trainingdb['heart']['w2']
+w3 = trainingdb['heart']['w3']
+w4 = trainingdb['ecmo']['w4']
+w5 = trainingdb['ecmo']['w5']
+w6 = trainingdb['ecmo']['w6']
+
+b1 = trainingdb['heart']['b1']
+b2 = trainingdb['heart']['b2']
+b3 = trainingdb['heart']['b3']
+b4 = trainingdb['ecmo']['b4']
+b5 = trainingdb['ecmo']['b5']
+b6 = trainingdb['ecmo']['b6']
+
+
+def NDivision(arr, n, ref):
+    outp = np.array([])
+    for i in range(0, len(arr)):
+        if i % n == ref:
+            outp = np.append(outp, arr[i])
+    return outp
+
+
+def DNN(x, h1_w, h1_b, h2_w, h2_b, o_w, o_b):
+    def Dnn_Relu(arr):
+        for i in range(0, arr.shape[0]):
+            if arr[i] < 0:
+                arr[i] = 0
+            else:
+                pass
+        return arr
+
+    z1 = np.matmul(x, h1_w) + h1_b
+    z1 = np.array(z1.reshape(z1.shape[0] * z1.shape[1]))
+    z1 = Dnn_Relu(z1)
+    z2 = np.matmul(z1, h2_w) + h2_b
+    z2 = np.array(z2.reshape(z2.shape[0] * z2.shape[1]))
+    z2 = Dnn_Relu(z2)
+    z = np.matmul(z2, o_w) + o_b
+    z = np.array(z.reshape(z.shape[0] * z.shape[1]))
+    outp = np.zeros(len(z))
+    outp[np.argmax(z)] = 1
+
+    return outp
+
+
+def differentiate(current_val, previous_val):
+    if previous_val is None:
+        return 0.0
+    else:
+        return (current_val - previous_val) * 0.004
+
+# Moving Average Filter
+# Savitzky-Golay Filter
+
+
+def savitzky_golay(y, window_size, order, deriv=0, rate=1):
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError as expt:
+        raise ValueError("window_size and order have to be of type int") from expt
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order + 1)
+    half_window = (window_size - 1) // 2
+    # precompute coefficients
+    b = np.mat([[k ** i for i in order_range] for k in range(-half_window, half_window + 1)])
+    m = np.linalg.pinv(b).A[deriv] * rate ** deriv * factorial(deriv)
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs(y[1:half_window + 1][::-1] - y[0])
+    lastvals = y[-1] + np.abs(y[-half_window - 1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve(m[::-1], y, mode='valid')
+
+
+ser = serial.Serial(port, baudrate, timeout=0.1)
+print('serial connect success')
+print(ser)
+
+ser.write(b'\x80\x73\x73\x8f')  # monitor에 's' 보내기
+
+R = ''
+ibp_diff = ''
+ibp_val = ''
+last_ibp_val = 0
+
+
+class DataPreprocessor(threading.Thread):
+    def __init__(self):
+        super().__init__()
+
+        self.ibp_diff_arr = ([])
+        self.ibp_wave_arr = ([])
+        self.flow_arr = ([])
+        self.pump1_arr = ([])
+        self.pump2_arr = ([])
+        self.serial_loop_n = 0
+
+    def run(self):
+        global R, ibp_val, last_ibp_val, ibp_diff
+
+        while 1:
+
+            if ser.readable():
+                a = ser.readline()
+
+                R += a.hex()
+
+                for i in range(0, len(R), 2):
+                    if R[i:i + 2] == '80':
+
+                        j_ref = 0
+                        for j in range(i + 2, len(R), 2):
+                            if R[j:j + 2] == '8f':
+
+                                # spo_h = R[i + 6:i + 8]
+                                # spo_l = R[i + 8:i + 10]
+                                #
+                                # spo_val = (int(spo_h, 16) & int('01111111', 2)) * (2 ** 7) + \
+                                #           (int(spo_l, 16) & int('01111111', 2)) - 1000
+
+                                ibp_h = R[i + 10:i + 12]
+                                ibp_l = R[i + 12:i + 14]
+
+                                ibp_val = (int(ibp_h, 16) & int('01111111', 2)) * (2 ** 7) + \
+                                          (int(ibp_l, 16) & int('01111111', 2)) - 512
+
+                                if ibp_val is not None:
+                                    ibp_diff = differentiate(ibp_val, last_ibp_val)
+                                    # print('Differentiated IBP value:', ibp_diff)
+                                    last_ibp_val = ibp_val
+                                else:
+                                    print('Error: ibp_val is None')
+
+                                flow_h = R[i + 18:i + 20]
+                                flow_l = R[i + 20:i + 22]
+
+                                flow_val = (int(flow_h, 16) & int('01111111', 2)) * (2 ** 7) + \
+                                          (int(flow_l, 16) & int('01111111', 2)) - 512
+
+                                sac_sig = R[i + 22:i + 24]
+
+                                pump_sig = format(int(sac_sig, 16), 'b').zfill(8)
+
+                                # pump1 = pump_sig[7]
+
+                                if pump_sig[7] == '0':
+                                    pump1 = 1
+                                else:
+                                    pump1 = 0
+
+                                # pump2 = pump_sig[6]
+
+                                if pump_sig[6] == '0':
+                                    pump2 = 1
+                                else:
+                                    pump2 = 0
+
+                                # print(pump1, pump2)
+
+                                if self.serial_loop_n % 4 == 0:
+
+                                    self.ibp_wave_arr = np.append(self.ibp_wave_arr, ibp_val)
+                                    if len(self.ibp_wave_arr) > 1000:
+                                        self.ibp_wave_arr = np.array(self.ibp_wave_arr[1::])
+                                    # print("ibp: ", *self.ibp_wave_arr)
+
+                                    self.ibp_diff_arr = np.append(self.ibp_diff_arr, ibp_diff)
+                                    if len(self.ibp_diff_arr) > 1000:
+                                        self.ibp_diff_arr = np.array(self.ibp_diff_arr[1::])
+                                    # print("ibp_diff: ", *self.ibp_diff_arr)
+
+                                    self.pump1_arr = np.append(self.pump1_arr, pump1)
+                                    if len(self.pump1_arr) > 1000:
+                                        self.pump1_arr = np.array(self.pump1_arr[1::])
+                                    # print('pump1: ', *self.pump1_arr)
+
+                                    self.pump2_arr = np.append(self.pump2_arr, pump2)
+                                    if len(self.pump2_arr) > 1000:
+                                        self.pump2_arr = np.array(self.pump2_arr[1::])
+                                    # print('pump2: ', *self.pump2_arr)
+
+                                    self.flow_arr = np.append(self.flow_arr, flow_val)
+                                    if len(self.flow_arr) > 1000:
+                                        self.flow_arr = np.array(self.flow_arr[1::])
+                                    # print("flow: ", *self.flow_arr)
+
+                                self.serial_loop_n += 1
+
+                                j_ref = 1
+                                break
+
+                        if j_ref == 0:
+                            R = R[i::]
+                            break
+
+
+save_outp_h = ''
+save_outp_e = ''
+
+
+
+class ApplyDNN(threading.Thread):
+    def __init__(self, parent):
+        super().__init__()
+
+        self.parent = parent
+
+        # self.ibp_wave_arr = ([])
+        # self.pump1_arr = ([])
+        # self.pump2_arr = ([])
+        # self.serial_loop_n = 0
+        self.dnn_loop_n = 0
+
+    def run(self):
+
+        global save_outp_h, save_outp_e
+        i = 0
+
+        print("DNN apply start")
+
+        while 1:
+
+            serial_loop_n = self.parent.serial_loop_n
+            ibp_wave_arr = self.parent.ibp_wave_arr
+            pump1_arr = self.parent.pump1_arr
+            pump2_arr = self.parent.pump2_arr
+
+            if self.dnn_loop_n >= serial_loop_n:
+                time.sleep(1)
+                continue
+
+            # index = -1 * (serial_loop_n - self.parent.dnn_loop_n)
+            index = len(ibp_wave_arr)
+
+            print('')
+            # print("thread2: ", len(ibp_wave_arr), len(pump1_arr), len(pump2_arr))
+            # print('index: ', index)
+            # print(serial_loop_n, self.dnn_loop_n)
+            if 1000 >= len(ibp_wave_arr) >= 125 and 1000 >= len(pump1_arr) >= 90 and 1000 >= len(pump2_arr) >= 90:
+
+                # print('thread2', len(ibp_wave_arr), 'pass')
+
+                # ibp_tmp = np.array(ibp_wave_arr[index - 125: index], dtype='float32')
+                # # pump1_tmp = np.array(pump1_arr[index - 125: index])
+                # # pump2_tmp = np.array(pump2_arr[index - 125: index])
+                # pump1_tmp = np.array(pump1_arr[index - 90: index], dtype='float32')
+                # pump2_tmp = np.array(pump2_arr[index - 90: index], dtype='float32')
+
+                ibp_tmp = np.array(ibp_wave_arr[index - 125:index], dtype='float32')
+                # pump1_tmp = np.array(pump1_arr[index - 125: index])
+                # pump2_tmp = np.array(pump2_arr[index - 125: index])
+                pump1_tmp = np.array(pump1_arr[index - 90:index], dtype='float32')
+                pump2_tmp = np.array(pump2_arr[index - 90:index], dtype='float32')
+
+                if len(ibp_wave_arr[index - 125:index]) <= 125 and len(pump1_arr[index - 90:index]) <= 90 and len(pump2_arr[index - 90:index]) <= 90:
+                    # print(ibp_tmp)
+
+                    # diff, sac1, sac2 각각 16분주 중 (div_i) 번째 분주 값 가져오기
+                    # for div_i in range(0, 1):
+                    #     diff = NDivision(ibp_tmp, 4, div_i)
+                    #     sac1 = NDivision(pump1_tmp, 4, div_i)
+                    #     sac2 = NDivision(pump2_tmp, 4, div_i)
+
+                    save_diff = np.array([])
+                    save_sac1 = np.array([])
+                    save_sac2 = np.array([])
+                    save_outp_h = np.array([])
+                    save_outp_e = np.array([])
+
+                    # diff 필요한 범위만 가져온 뒤 표준화
+                    # diff_inp = ibp_tmp[index - 125:index]
+                    diff_inp = np.array(ibp_tmp)
+                    # save_diff_val = np.array(diff_inp)
+
+                    # if len(diff_inp) > 0:
+                    #     diff_inp_min = np.min(diff_inp)
+                    # else:
+                    #     diff_inp_min = 0
+
+                    # diff_inp_min = np.min(diff_inp)
+                    # diff_inp_max = np.max(diff_inp)
+
+                    diff_inp_min = np.min(diff_inp) if len(diff_inp) > 0 else 0
+                    diff_inp_max = np.max(diff_inp) if len(diff_inp) > 0 else 0
+
+                    diff_inp = (diff_inp - diff_inp_min) / (diff_inp_max - diff_inp_min)
+
+                    # if diff_inp_max == diff_inp_min:
+                    #     diff_inp = np.zeros_like(diff_inp)
+                    # else:
+                    #     diff_inp = (diff_inp - diff_inp_min) / (diff_inp_max - diff_inp_min)
+
+                    # print(diff_inp)
+
+                    # sac1 필요한 범위만 가져오기
+                    # sac1_inp = pump1_tmp[index - 90:index]
+                    sac1_inp = np.array(pump1_tmp)
+                    save_sac1_val = np.array(sac1_inp)
+                    # for sac_i in range(0, len(sac1_inp)):
+                    #     if sac1_inp[sac_i] >= 0.5:
+                    #         sac1_inp[sac_i] = 1
+                    #     else:
+                    #         sac1_inp[sac_i] = 0
+
+                    # sac2 필요한 범위만 가져오기
+                    # sac2_inp = pump2_tmp[index - 90:index]
+                    sac2_inp = np.array(pump2_tmp)
+                    save_sac2_val = np.array(sac2_inp)
+                    # for sac_i in range(0, len(sac2_inp)):
+                    #     if sac2_inp[sac_i] >= 0.5:
+                    #         sac2_inp[sac_i] = 1
+                    #     else:
+                    #         sac2_inp[sac_i] = 0
+
+                    inp = np.hstack((diff_inp, sac1_inp, sac2_inp))
+
+                    # DNN 적용
+                    outp_h = DNN(inp, w1, b1, w2, b2, w3, b3)
+                    outp_e = DNN(inp, w4, b4, w5, b5, w6, b6)
+                    # print('dnn pass')
+                    # DNN 출력값 중 마지막 값(파형 없음을 나타내는) 제거
+                    outp_h = np.array(outp_h[0:-1])
+                    outp_e = np.array(outp_e[0:-1])
+
+                    # 출력값 누적
+                    # if i == 0:
+                    #     save_diff = np.array(diff_inp)
+                    #     save_sac1 = np.hstack((np.zeros(35), save_sac1_val))
+                    #     save_sac2 = np.hstack((np.zeros(35), save_sac2_val))
+                    #     save_outp_h = np.hstack((np.zeros(65), outp_h, np.zeros(30)))
+                    #     save_outp_e = np.hstack((np.zeros(65), outp_e, np.zeros(30)))
+                    # else:
+                    #     save_diff = np.append(save_diff, diff_inp[-1])
+                    #     save_sac1 = np.append(save_sac1, sac1_inp[-1])
+                    #     save_sac2 = np.append(save_sac2, sac2_inp[-1])
+                    #
+                    #     save_outp_h = np.append(save_outp_h, 0)
+                    #     save_outp_h[-60:-30] = save_outp_h[-60:-30] + outp_h
+                    #
+                    #     save_outp_e = np.append(save_outp_e, 0)
+                    #     save_outp_e[-60:-30] = save_outp_e[-60:-30] + outp_e
+
+                    # 출력값 누적
+                    if i == 0:
+                        save_diff = np.array(diff_inp)
+                        save_sac1 = np.hstack((np.zeros(35), save_sac1_val))
+                        save_sac2 = np.hstack((np.zeros(35), save_sac2_val))
+                        save_outp_h = np.hstack((np.zeros(65), outp_h, np.zeros(30)))
+                        save_outp_e = np.hstack((np.zeros(65), outp_e, np.zeros(30)))
+                    else:
+                        save_diff = np.append(save_diff, diff_inp[-1])
+                        save_sac1 = np.append(save_sac1, sac1_inp[-1])
+                        save_sac2 = np.append(save_sac2, sac2_inp[-1])
+
+                        save_outp_h = np.append(save_outp_h, 0)
+                        save_outp_h[-60:-30] = save_outp_h[-60:-30] + outp_h
+
+                        save_outp_e = np.append(save_outp_e, 0)
+                        save_outp_e[-60:-30] = save_outp_e[-60:-30] + outp_e
+
+                    a1 = save_outp_h[-60]
+                    a2 = save_outp_e[-60]
+                    print(a1, a2)
+
+                    self.dnn_loop_n += 1
+
+                else:
+                    pass
+
+            else:
+                pass
+
+
+class MyProgram:
+    def __init__(self):
+
+        # self.ibp_wave_arr = np.array([])
+        # self.pump1_arr = np.array([])
+        # self.pump2_arr = np.array([])
+        # self.serial_loop_n = 0
+        # self.dnn_loop_n = 0
+
+        self.thread1 = DataPreprocessor()
+        self.thread2 = ApplyDNN(self.thread1)
+
+    def run(self):
+        self.thread1.start()
+        self.thread2.start()
+
+        self.thread1.join()
+        self.thread2.join()
+
+    # def stop(self):
+    #     self.thread1.stop()
+    #     self.thread2.stop()
+
+
+my_program = MyProgram()
+my_program.run()
+# time.sleep(1)
